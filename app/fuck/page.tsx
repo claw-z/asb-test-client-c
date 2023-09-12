2499b17b6cd6f7e15b205d3f22c1a7b2116b5e9e
claw-z <you@example.com>
2023-09-12 12:16:52 +0200

    feat: fetch data via endpoint work in progress

--- app/fuck/page.tsx
+++ app/fuck/page.tsx
@@ -1,7 +1,27 @@
-export default function FuckPage() {
+'use client';
+
+import { SecondChart } from '@/components/SecondChart';
+import { MeasurementHttpClient } from '@/lib/http-client/endpoints/measurement';
+
+export default async function FuckPage() {
+  const endPoint = new MeasurementHttpClient();
+  const measurement = await endPoint.getMeasurement('ecd2b436-9454-4a50-8c25-0636d5a98f19')
+  console.log(measurement)
+
+  // const data = await fetch(
+  //   'http://localhost:5000/api/impedance/8614fd32-7e7c-4f97-9e24-920e44962d21',
+  //   {
+  //     method: 'GET',
+  //     headers: {
+  //       Authorization: 'asb',
+  //       'content-type': 'application/json'
+  //     }
+  //   }
+  // );
+
   return (
     <div>
-      <h1>Fucque</h1>
+      <SecondChart></SecondChart>
     </div>
   );
 }
---
+++ components/SecondChart.tsx
@@ -0,0 +1,20 @@
+import 'chart.js/auto';
+import { Chart } from 'react-chartjs-2';
+
+export const SecondChart = () => {
+  
+  return (
+    <Chart
+      type="line"
+      data={{
+        labels: [1, 2, 3, 4, 5],
+        datasets: [
+          {
+            data: [6, 7, 8, 9, 11],
+            backgroundColor: 'blue'
+          }
+        ]
+      }}
+    />
+  );
+};
