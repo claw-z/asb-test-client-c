4f85fc441081ef9c73960bb9f973a74c5e8b653d
claw-z <you@example.com>
2023-09-12 15:59:01 +0200

    chore: data fetching WIP

--- app/fuck/page.tsx
+++ app/fuck/page.tsx
@@ -3,21 +3,30 @@
 import { SecondChart } from '@/components/SecondChart';
 import { MeasurementHttpClient } from '@/lib/http-client/endpoints/measurement';
 
-export default async function FuckPage() {
-  const endPoint = new MeasurementHttpClient();
-  const measurement = await endPoint.getMeasurement('ecd2b436-9454-4a50-8c25-0636d5a98f19')
-  console.log(measurement)
+async function getData() {
+  const res = await fetch(
+    'http://localhost:5000/api/measurement/30a56c2e-152c-462b-8e7a-fd3586cc0b69'
+  );
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
 
-  // const data = await fetch(
-  //   'http://localhost:5000/api/impedance/8614fd32-7e7c-4f97-9e24-920e44962d21',
-  //   {
-  //     method: 'GET',
-  //     headers: {
-  //       Authorization: 'asb',
-  //       'content-type': 'application/json'
-  //     }
-  //   }
-  // );
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function FuckPage() {
+  
+  const data = await getData();
+  console.log(data);
+
+  const endPoint = new MeasurementHttpClient();
+  const measurement = await endPoint.getMeasurement(
+    'ecd2b436-9454-4a50-8c25-0636d5a98f19'
+  );
+  console.log(measurement);
 
   return (
     <div>
