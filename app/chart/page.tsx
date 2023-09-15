1dc5f33eee0f5e9d5758a11d20ee1174b4eff633
claw-z <you@example.com>
2023-09-15 12:30:17 +0200

    chore: current status

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -1,8 +1,28 @@
-"use client";
+'use client';
 
-import { FirstChart } from "@/components/FirstChart";
+import { useState, useEffect } from 'react';
+import { FirstChart } from '@/components/FirstChart';
 
-export default function ChartJS({ params }: any) {
+export default function ChartJS() {
+  const [measurement, setMeasurement] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      'http://localhost:5000/api/measurement/ecd2b436-9454-4a50-8c25-0636d5a98f19',
+      {
+        method: 'GET',
+        headers: {
+          Authorization: 'asb',
+          'content-type': 'application/json'
+        }
+      }
+    )
+      .then(res => res.json())
+      .then(data => setMeasurement(data));
+  }, ['ecd2b436-9454-4a50-8c25-0636d5a98f19']);
+
+  console.log('CHART PAGE:', measurement);
+
   return (
     <div>
       <FirstChart></FirstChart>
